b578aa53eac9042e7192e0f92f9d0f65f951dbfc
skusman09 <imusman46@gmail.com>
2026-04-11 10:23:09 +0530

    chore: optimize for cloudflare pages production deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://staffone.io',
+  output: 'hybrid',
+  adapter: cloudflare(),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/cloudflare'
+    }
+  },
   vite: {
     css: {
       preprocessorOptions: {}
